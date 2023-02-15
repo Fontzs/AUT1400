d68fa34ba70969236657c76d55bd45fb5cf75e30
Franz <2simple233@gmail.com>
2023-02-15 20:27:59 +0800

    finish a part of hw1

--- HW/AP1400-2-HW1/include/hw1.h
+++ HW/AP1400-2-HW1/include/hw1.h
@@ -1,5 +1,49 @@
+
 #ifndef AP_HW1_H
 #define AP_HW1_H
 
+namespace algebra {
 
-#endif //AP_HW1_H
+using Matrix = std::vector<std::vector<double>>;
+
+Matrix zeros(size_t n, size_t m);
+
+Matrix ones(size_t n, size_t m);
+
+Matrix random(size_t n, size_t m, double min, double max);
+
+void show(const Matrix& matrix);
+
+Matrix multiply(const Matrix& matrix, double c);
+
+Matrix multiply(const Matrix& matrix1, const Matrix& matrix2);
+
+Matrix sum(const Matrix& matrix, double c);
+
+Matrix sum(const Matrix& matrix1, const Matrix& matrix2);
+
+Matrix transpose(const Matrix& matrix);
+
+Matrix minor(const Matrix& matrix, size_t n, size_t m);
+
+double determinant(const Matrix& matrix);
+
+Matrix inverse(const Matrix& matrix);
+
+Matrix concatenate(const Matrix& matrix1, const Matrix& matrix2, int axis = 0);
+
+Matrix inverse(const Matrix& matrix);
+
+Matrix concatenate(const Matrix& matrix1, const Matrix& matrix2, int axis = 0);
+
+Matrix ero_swap(const Matrix& matrix, size_t r1, size_t r2);
+
+Matrix ero_multiply(const Matrix& matrix, size_t r, double c);
+
+Matrix ero_sum(const Matrix& matrix, size_t r1, double c, size_t r2);
+
+Matrix upper_triangular(const Matrix& matrix);
+
+}  // namespace algebra
+
+#endif  // AP_HW1_H
--- HW/AP1400-2-HW1/src/hw1.cpp
+++ HW/AP1400-2-HW1/src/hw1.cpp
@@ -1,1 +1,90 @@
 #include "hw1.h"
+
+#include <iomanip>
+#include <random>
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+namespace algebra {
+
+Matrix CreateMatrix(size_t n, size_t m, double v) {
+    Matrix x;
+    for (int i = 0; i < n; i++) {
+        std::vector<double> vect(m, v);
+        x.push_back(vect);
+    }
+    return x;
+}
+
+Matrix zeros(size_t n, size_t m) { return CreateMatrix(n, m, 0); }
+
+Matrix ones(size_t n, size_t m) { return CreateMatrix(n, m, 1); }
+
+Matrix random(size_t n, size_t m, double min, double max) {
+    Matrix x;
+    std::default_random_engine e;
+    std::uniform_real_distribution<double> u(min, max);
+
+    for (int i = 0; i < n; i++) {
+        std::vector<double> vect;
+        for (int j = 0; j < m; j++) {
+            vect.push_back(u(e));
+        }
+        x.push_back(vect);
+    }
+    return x;
+}
+
+void show(const Matrix& matrix) {
+    int m = matrix.size();
+    int n = matrix[0].size();
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << left << setw(5) << setprecision(3) << matrix[i][j];
+        }
+        cout << endl;
+    }
+}
+
+Matrix multiply(const Matrix& matrix, double c) {
+    int m = matrix.size();
+    int n = matrix[0].size();
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            matrix[i][j] *= c;
+        }
+    }
+    return matrix;
+}
+
+Matrix multiply(const Matrix& matrix1, const Matrix& matrix2) {}
+
+Matrix sum(const Matrix& matrix, double c) {
+    int m = matrix.size();
+    int n = matrix[0].size();
+    for (int i = 0; i < m; i++) {
+        for (int j = 0; j < n; j++) {
+            matrix[i][j] += c;
+        }
+    }
+    return matrix;
+}
+
+Matrix sum(const Matrix& matrix1, const Matrix& matrix2) {
+    int m{matrix1.size()};
+    int n{matrix1[0].size()};
+    Matrix x(m);
+    for (int i{0}; i < m; i++) {
+        std::vector<double> vect(n);
+        for (int j{0}; j < n; j++) {
+            vect.push_back(matrix1[i][j] + matrix2[i][j]);
+        }
+        x.push_back(vect);
+    }
+
+    return x;
+}
+
+}  // namespace algebra
